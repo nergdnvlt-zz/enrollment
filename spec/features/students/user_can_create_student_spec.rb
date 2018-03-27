12ddc2a3dc37e1611de1d0c6b776d5dd02861c76
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-27 13:49:11 -0600

    Added TDD for new student.

---
+++ spec/features/students/user_can_create_student_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'User can create student' do
+  describe 'user fills form to create student' do
+    it 'sucessfully adds student' do
+      visit new_student_path
+
+      fill_in 'student[name]', with: 'Ace High'
+
+      expect(page).to have_content('Ace High')
+    end
+  end
+end
