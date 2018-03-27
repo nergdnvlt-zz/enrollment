101559a445dce241a1451d9dd9c2c0b8956515f7
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-27 14:56:23 -0600

    Added foreign key to addresses, nested address resources, and added model relationship.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,8 @@ describe Student do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe 'Relationships' do
+    it { should have_many(:addresses) }
+  end
 end
